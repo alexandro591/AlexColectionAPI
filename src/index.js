a1a0749d250b204439b5eeffce603765487759dc
“alexandro591” <alexandrotapiaflores@gmail.com>
2020-01-14 13:11:54 -0500

    jeje

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const nodemailer = require("nodemailer")
-const { EMAIL_USER, EMAIL_PASS } = process.env
 
 alexColectionAPI.use(bodyParser.json());
 alexColectionAPI.use(bodyParser.urlencoded({ extended: true }));
@@ -19,34 +18,11 @@ alexColectionAPI.use(function(req, res, next) {
 
 
 router.get("/",function(request,response){
-    var transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-          user: EMAIL_USER, // generated ethereal user
-          pass: EMAIL_PASS // generated ethereal password
-        }
-    });
-
-    transporter.sendMail({
-        from: EMAIL_USER,
-        to: 'alexandrotapiaflores@gmail.com',
-        subject: 'notifymelocalhost@gmail.com',
-        text: 'hola mundo'
-    }, function(error, info) {
-    	if (error) {
-            callback(error);
-            response.write(error.toString())
-    	} else {
-    		callback(null, {
-			    statusCode: 200,
-			    body: "Ok"
-	    	});
-    	}
-    });
-    response.write(EMAIL_USER+"\n"+EMAIL_PASS);
-    response.end();
+    axios.get("https://sistemaunico.ant.gob.ec:5038/PortalWEB/paginas/clientes/clp_grid_citaciones.jsp?ps_tipo_identificacion=CED&ps_identificacion=1500655533&ps_placa=")
+    .then(body=>{
+        response.write(body.data)
+        response.end();
+    })
 });
 
 
